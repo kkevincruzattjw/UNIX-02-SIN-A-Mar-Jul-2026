1e0c2fe31c6f4212e7a42416bd0c6309d5c11ef7
kcruzate <kcruzate@example.com>
2026-05-31 15:13:47 -0500

    feat: initialize script with headers

---
+++ script_linux_cisco_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script: script_linux_cisco_2.sh
+# Description: Documenting commands executed during the NDG Linux Unhatched course (Part 2).
+# Author: kkevincruzattjw
+# Date: 2026-05-31
