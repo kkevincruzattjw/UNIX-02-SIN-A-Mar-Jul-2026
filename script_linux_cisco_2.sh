0fbace2532056611b230add19797db91d0731c39
kcruzate <kcruzate@example.com>
2026-05-31 15:13:57 -0500

    docs: add moving and removing files sections

--- script_linux_cisco_2.sh
+++ script_linux_cisco_2.sh
@@ -3,3 +3,24 @@
 # Description: Documenting commands executed during the NDG Linux Unhatched course (Part 2).
 # Author: kkevincruzattjw
 # Date: 2026-05-31
+
+# --- MODULE: MOVING FILES ---
+
+# Rename a file from source_name to target_name
+# Example: Rename 'old_name.txt' to 'new_name.txt'
+mv old_name.txt new_name.txt
+
+# Move a file to another directory
+# Example: Move 'new_name.txt' to the '/tmp' directory
+mv new_name.txt /tmp/
+
+# --- MODULE: REMOVING FILES ---
+
+# Remove (delete) a file
+# Example: Delete 'unwanted_file.txt'
+rm unwanted_file.txt
+
+# Remove a directory and its contents recursively
+# Example: Delete a directory named 'temp_folder' and everything inside it
+rm -r temp_folder
+
