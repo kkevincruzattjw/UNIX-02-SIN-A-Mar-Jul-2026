3a2c3d2670241ab58390efb994f489617cab45f8
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-06-04 13:00:45 +0000

    commit 2

--- blackhatbash.sh
+++ blackhatbash.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Displays the current version of Bash you are running, along with license information.
 bash --version
 #Lists all the environment variables in your system (like PATH, USER, HOME), which define the behavior of your current session.
@@ -8,3 +9,10 @@ echo ${SHELL}
 echo ${UID}
 #Prints the type of operating system currently running the shell (e.g., linux-gnu or darwin23.0).
 echo ${OSTYPE}
+#Displays a full-format list of active processes in the current terminal, showing extra details like the owner (UID), parent process (PPID), and the exact command used.
+ps -f
+#Displays a full-format list of active processes in the current terminal, showing extra details like the owner (UID) and parent process (PPID).
+bash -n blackhatbash.sh
+#Checks for syntax errors in the script without actually running it. It reads the code but does not execute any commands.
+bash -x blackhatbash.sh
+#Runs the script in debug mode. It prints each command to the screen exactly as it is executed, showing how variables expand, which helps you find bugs.
