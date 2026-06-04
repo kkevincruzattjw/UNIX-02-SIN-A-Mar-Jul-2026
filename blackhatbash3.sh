0899aae4030652e574a04c63dcde83697d537ae5
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-06-04 13:24:06 +0000

    blackhatbahs3

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
 book="black hat bash"
-echo "This book's name is ${book}
+echo "This book's name is ${book}"
+
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+unset book
+echo "$BOOK
