22bf3aa1f48d9e7c92758d4a461abbe425dc5340
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-06-11 12:57:25 +0000

    string _comparison.sh

---
+++ string_comparison.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VARIABLE_ONE="nostarch1"
+VARIABLE_TWO="nostarch"
+
+if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
+    echo "They are equal!"
+else
+    echo "They are not equal!"
+fi
